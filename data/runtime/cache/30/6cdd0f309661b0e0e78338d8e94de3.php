7ddffba4acb99db8fd1114eba314256b209e18f0
SunYingbo <846413979@qq.com>
2024-11-15 09:14:08 +0800

    gitignore

--- data/runtime/cache/30/6cdd0f309661b0e0e78338d8e94de3.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:10:{s:9:"site_name";s:42:"Henan Mining Crane Import &amp; Export Co.";s:14:"site_seo_title";s:30:"Henan Kuangshan Crane Co.,Ltd.";s:17:"site_seo_keywords";s:30:"Henan Kuangshan Crane Co.,Ltd.";s:20:"site_seo_description";s:30:"Henan Kuangshan Crane Co.,Ltd.";s:8:"sale_tel";s:16:"+86-0373-3088011";s:14:"after_sale_tel";s:16:"+86-0373-3088011";s:8:"site_gwa";s:22:"豫ICP备16003949号-1";s:9:"copyright";s:41:"Copyright©Henan Kuangshan Crane Co.,Ltd.";s:14:"site_analytics";s:0:"";s:5:"email";s:18:"info@hukscrane.com";}
--- data/runtime/cache/ad/beed380c67b504412a25e8c84ae90f.php
+++
@@ -1,4 +0,0 @@
-<?php
-//000000000000
- exit();?>
-a:3:{i:0;s:91:"E:\product\kuangshan\kuangshan-cmf\data\runtime\cache\7c\fd7023fe239ca7e38dac18fc978562.php";i:1;s:91:"E:\product\kuangshan\kuangshan-cmf\data\runtime\cache\dc\2d5460e4313a320b8775be8815268f.php";i:2;s:91:"E:\product\kuangshan\kuangshan-cmf\data\runtime\cache\5d\488968ca96416815ab34c96e65cf3a.php";}
